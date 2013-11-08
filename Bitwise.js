1d9aeb945d10c9d62ee31f2f453faa5a9db01841
liamondrop <liamondrop@gmail.com>
2013-11-08 13:18:58 -0500

    out of range error handling

--- Bitwise.js
+++ Bitwise.js
@@ -12,6 +12,12 @@
     return _zeroPad(n.toString(16), 2)
   }
 
+  function _checkRange(range) {
+    for (var args = Array.prototype.slice.apply(arguments), i = 1, l = args.length, a; i < l; i++)
+      if (a = args[i], a < range[0] || a > range[1])
+        throw new RangeError('value (' + a + ') out of range [' + range.join(' .. ') + ']')
+  }
+
   var trans  = 8192,   // used to translate the range to non-negative land
       lomask = 0x00ff, // 0000000011111111 = 255
       himask = 0xff00; // 1111111100000000 = 65280
@@ -24,6 +30,7 @@
      * n: an integer (base 10) in the range [-8192 .. 8191]
      */
     encode: function (n) {
+      _checkRange([-8192, 8191], n);
       var hi, lo;
       n  = (n + trans)  << 1; // translate & left-shift 1 bit to clear msb
       hi = (n & himask) >> 8; // hi byte
@@ -37,6 +44,7 @@
      * hi, lo: integers (base 16) in the range [0x00 .. 0x7f]
      */
     decode: function (hi, lo) {
+      _checkRange([0x00, 0x7f], hi, lo);
       hi = hi << 8; // unpack the hi byte
       lo = lo << 1; // unpack the lo byte
       return ((hi | lo) >> 1) - trans; // hi & lo unioned, right-shifted & returned to original position
