eb8e14c1fc498e9b605420eb0c1c5624eeec5bfd
liamondrop <liamondrop@gmail.com>
2013-11-08 12:27:34 -0500

    little more cleanup

--- Bitwise.js
+++ Bitwise.js
@@ -8,7 +8,7 @@
     return n;
   }
 
-  function _toHex(n) {
+  function _toHexStr(n) {
     return _zeroPad(n.toString(16), 2)
   }
 
@@ -25,23 +25,21 @@
      */
     encode: function (n) {
       var hi, lo;
-      n  = (n + trans)  << 1; // translate & left shift 1 bit to clear msb
+      n  = (n + trans)  << 1; // translate & left-shift 1 bit to clear msb
       hi = (n & himask) >> 8; // hi byte
       lo = (n & lomask) >> 1; // lo byte
-      return _toHex(hi) + _toHex(lo);
+      return _toHexStr(hi) + _toHexStr(lo);
     },
 
     /**
      * decode
      * accepts a pair of hex values and returns a decoded integer
-     * hi, lo: integers (base 16) formatted as [0x00 .. 0x7f]
-     * n: pointer
+     * hi, lo: integers (base 16) in the range [0x00 .. 0x7f]
      */
-    decode: function (hi, lo, n) {
-      lo = lo << 1;       // unpack the lo byte
-      hi = hi << 8;       // unpack the hi byte
-      n = (hi | lo) >> 1; // hi & lo combined & right shifted
-      return n - trans;   // return range to original position
+    decode: function (hi, lo) {
+      hi = hi << 8; // unpack the hi byte
+      lo = lo << 1; // unpack the lo byte
+      return ((hi | lo) >> 1) - trans; // hi & lo unioned, right-shifted & returned to original position
     }
   };
 
